e19d29956b70b487d3e3d1255e338eb32d7a847b
Kostis Anagnostopoulos <ankostis@gmail.com>
2023-05-19 01:10:52 +0300

    FEAT(PYTEST) Pytest setup; +TCs for Menu

---
+++ tests/test_workmanship.py
@@ -0,0 +1,29 @@
+import pytest
+
+import workmanship as wm
+
+
+@pytest.mark.parametrize(
+    "item, exp",
+    [
+        ("a", {"1": ("a", None)}),
+        (("a", None), {"1": ("a", None)}),
+        (("1", "a", None), {"1": ("a", None)}),
+        (("2", "a", None), {"2": ("a", None)}),
+        ({("a"): None}, {"1": ("a", None)}),
+        ({("1", "a"): None}, {"1": ("a", None)}),
+        ({("2", "a"): None}, {"2": ("a", None)}),
+        ({("Q", "Quit"): None}, {"q": ("Quit", None)}),
+        (1, ValueError("Invalid")),
+        ((1, "dd"), ValueError("Invalid")),
+        (("k", 12), {"1": ("k", 12)}),
+        ({(2, "a"): None}, ValueError("Invalid")),
+    ],
+)
+def test_menu(item, exp):
+    if isinstance(exp, Exception):
+        with pytest.raises(type(exp), match=str(exp)):
+            wm.menu_records(item)
+    else:
+        menu = wm.menu_records(item)
+        assert menu == exp
